6c8a70647fef1d6d37d63acd36f3fb0523d42a5d
naimuddin94 <developernaimuddin@gmail.com>
2023-09-15 00:57:16 +0600

    selection logical build is complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,40 @@ import Selection from "./components/Selection";
 
 function App() {
   const [selectionList, setSelectionList] = useState([]);
+  const [remainingTime, setRemainingTime] = useState(20);
+  const [totalTime, setTotalTime] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const handleSelection = (course) => {
-    setSelectionList([...selectionList, course]);
+    const selected = selectionList.find((sCourse) => sCourse.id === course.id);
+    if (selected) {
+      return alert("Course already selected");
+    } else {
+      if (remainingTime < course.time) {
+        return alert("You have no enough time");
+      }
+      setTotalTime(totalTime + course.time);
+      setRemainingTime(remainingTime - course.time);
+      setSelectionList([...selectionList, course]);
+      setTotalPrice(totalPrice + course.price);
+    }
   };
+
+  console.log(totalTime, remainingTime);
 
   return (
     <div className="container mx-auto px-5 md:px-8">
       <h1 className="text-2xl text-slate-700 font-bold text-center py-3">
         Course Registration
       </h1>
-      <div className="md:grid grid-cols-12 gap-4">
+      <div className="md:grid grid-cols-12 gap-4 mt-5">
         <Courses handleSelection={handleSelection} />
-        <Selection selectionList={selectionList} />
+        <Selection
+          selectionList={selectionList}
+          remainingTime={remainingTime}
+          totalTime={totalTime}
+          totalPrice={totalPrice}
+        />
       </div>
     </div>
   );
--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -1,29 +1,30 @@
 import PropTypes from "prop-types";
 
-const Selection = ({ selectionList }) => {
+const Selection = ({ selectionList, remainingTime, totalTime, totalPrice }) => {
   return (
     <div className="col-span-3">
       <div className="bg-white px-5 py-6 rounded-lg">
         <h2 className="font-bold text-base text-[#2F80ED] py-2">
-          Credit Hour Remaining 7 hr
+          Credit Hour Remaining {remainingTime} hr
         </h2>
         <hr />
         <h1 className="font-bold text-lg text-slate-800 py-2">Course Name</h1>
         <div className="py-2">
           {selectionList &&
             selectionList.map((course, index) => (
-              <p key={index} className="py-1">{`${index + 1} ${
-                course.title
-              }`}</p>
+              <p key={index} className="py-1 text-[0.95rem]">
+                <span className="font-semibold">{index + 1}.</span>{" "}
+                {course.title}
+              </p>
             ))}
         </div>
         <hr />
         <h2 className="text-slate-600 font-medium py-2">
-          Total Credit Hour : 13
+          Total Credit Hour : {totalTime}
         </h2>
         <hr />
         <h1 className="text-slate-600 font-semibold py-2">
-          Total Price : 48000 USD
+          Total Price : {totalPrice} USD
         </h1>
       </div>
     </div>
@@ -31,7 +32,10 @@ const Selection = ({ selectionList }) => {
 };
 
 Selection.propTypes = {
-  selectionList: PropTypes.array,
+  selectionList: PropTypes.array.isRequired,
+  remainingTime: PropTypes.number.isRequired,
+  totalTime: PropTypes.number.isRequired,
+  totalPrice: PropTypes.number.isRequired,
 };
 
 export default Selection;
